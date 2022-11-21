7b2f4242c0cfd70bced3ac0ce31f76ed146e8230
KrishDhungana <kdhungana@hawk.iit.edu>
2022-11-21 17:39:54 -0600

    Adding In API
    Adding in map and markers for API

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function initMap() {
+	const myLatLng = { lat:48.8566, lng:2.3522};
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 4,
+		center: myLatLng,
+	});
+
+	new google.maps.Marker({
+    	position: myLatLng,
+    	map,
+ 	 });
+
+
+	new google.maps.Marker({
+        position: new google.maps.LatLng(45.47859954420433, 9.227346521619015),
+        map,
+        animation: google.maps.Animation.DROP
+	});
+	
+	new google.maps.Marker({
+        position: new google.maps.LatLng(51.5072,0.1276),
+        map,
+        animation: google.maps.Animation.DROP
+	});
+
+}
+
+window.initMap = initMap;
